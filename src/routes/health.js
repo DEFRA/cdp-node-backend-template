86498bec773f1e56e8d3bfee46888c617683335e
John Watson <john.watson1@defra.gov.uk>
2025-03-03 11:48:15 +0000

    Remove default status

--- src/routes/health.js
+++ src/routes/health.js
@@ -1,9 +1,7 @@
-import { constants } from 'http2'
-
 const health = {
   method: 'GET',
   path: '/health',
-  handler: (_request, h) => h.response({ message: 'success' }).code(constants.HTTP_STATUS_OK)
+  handler: (_request, h) => h.response({ message: 'success' })
 }
 
 export { health }
