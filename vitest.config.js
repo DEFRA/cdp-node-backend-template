d4f7de3898d74d48e4b2577a1d13db3dfb5e7e5a
Ben Chidgey <ben@feedmypixel.com>
2025-12-18 12:10:03 +0000

    CORE-2055: Bump dependencies and Node.js
    - Add in npm run security-audit

--- vitest.config.js
+++ vitest.config.js
@@ -5,11 +5,12 @@ export default defineConfig({
     globals: true,
     environment: 'node',
     clearMocks: true,
+    fileParallelism: false,
     coverage: {
       provider: 'v8',
       reportsDirectory: './coverage',
       reporter: ['text', 'lcov'],
-      include: ['src/**'],
+      include: ['src/**/*.js'],
       exclude: [...configDefaults.exclude, 'coverage']
     },
     setupFiles: ['.vite/mongo-memory-server.js', '.vite/setup-files.js']
